6812c816cf3f1696db5c9250a52a0d04597adf54
EricHanJf <jf08942378268@gmail.com>
2025-10-22 13:07:38 +0100

    :rocket: Add services to the container.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // builder.Services.AddScoped<CountryService>();
 builder.Services.AddHttpClient<CountryService>();
+builder.Services.AddHttpClient<WeatherService>();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
